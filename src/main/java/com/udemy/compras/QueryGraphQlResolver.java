69de55552882aed3dfdc3f9f11a6ff6328333bb4
Mel <melmelissa1936@gmail.com>
2023-04-16 13:49:13 -0300

    feat: add new example methods graphql

---
+++ src/main/java/com/udemy/compras/QueryGraphQlResolver.java
@@ -0,0 +1,33 @@
+package com.udemy.compras;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.checkerframework.checker.units.qual.C;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueryGraphQlResolver implements GraphQLQueryResolver {
+
+    public String hello() {
+        return "Hello GraphQl";
+    }
+
+    public int soma(int a, int b) {
+        return a + b;
+    }
+
+    public Cliente cliente() {
+        return new Cliente("Mel", "mel@test.com");
+    }
+
+    public List<Cliente> clientes() {
+        final List<Cliente> clientes = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            clientes.add(new Cliente("Cliente " + i, "mail" + i + "test.com"));
+        }
+        return clientes;
+    }
+
+}
